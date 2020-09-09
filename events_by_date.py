7c5aa978b3b20a78ac145be4b4fc2e4f8ba67406
gosols <teemu.a.kosonen@hotmail.com>
2020-09-09 09:31:52 +0300

    commit

--- events_by_date.py
+++ events_by_date.py
@@ -1,33 +1,37 @@
 import urllib.request as r
 import json
 
+# fetch JSON data
 url = r.urlopen("http://open-api.myhelsinki.fi/v1/events/")
 html = url.read()
 jsonObj = json.loads(html)
 events = jsonObj["data"]
 
 
-# algorithm found from the internets
-def bubbleSort(arr):
-    n = len(arr)
-    # Traverse through all array elements
+# algorithm found from the internets - Bubble Sort Algorithm
+def bubbleSort(list):
+    n = len(list)
+    # Iterate through every event in list
     for i in range(n-1):
         # range(n) also work but outer loop will repeat one time more than needed.
 
-        # Last i elements are already in place
         for j in range(0, n-i-1):
-            current_event = arr[j]["event_dates"]["starting_day"]
-            next_event = arr[j+1]["event_dates"]["starting_day"]
+            # variables for cleaner code
+            current_event = list[j]["event_dates"]["starting_day"]
+            next_event = list[j+1]["event_dates"]["starting_day"]
 
+            # if there's no date given to the current event
             if current_event is None:
                 current_event = "no date"
-                arr[j]["event_dates"]["starting_day"] = "No Date"
+                list[j]["event_dates"]["starting_day"] = "No Date"
+            # if there's no date given to the next event
             if next_event is None:
                 next_event = "no date"
-                arr[j+1]["event_dates"]["starting_day"] = "No Date"
+                list[j+1]["event_dates"]["starting_day"] = "No Date"
 
+            # if current (date) is bigger (later), put it below the next event
             if current_event > next_event:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+                list[j], list[j+1] = list[j+1], list[j]
 
 
 # sort events by date (and time)
@@ -35,8 +39,10 @@ bubbleSort(events)
 
 current_date = ""
 i = 0
+# outer for loop for outputting dates
 for event in events:
 
+    # put the date in a variable (YYY-MM-DD)
     date = event["event_dates"]["starting_day"][0:10]
     if date != current_date:
 
@@ -44,7 +50,7 @@ for event in events:
         print("")
         print(current_date)
         print("----------")
-
+    # iterate through every event that has a matching date
     while i < len(events):
         name = events[i]["name"]["fi"]
         time = events[i]["event_dates"]["starting_day"][11:16]
